55cb523768fab546a266669fd5c5b84fd0967c6a
xNombre <kartapolska@gmail.com>
2019-02-03 22:35:15 +0100

    Remove dynamic fsync
    Prepare for new Block Tuner driver.
    
    Revert "fs: dynamic_fsync: convert to fb notifier"
    
    This reverts commit a4499acc750da3f1dde0ed679fea840b09f11b2f.
    
    Revert "fs: dynamic_fsync: switch to native linux functions"
    
    This reverts commit b0efd7a29f89ffef1312a46c33bb8b46cf50f41f.
    
    Revert "fs: dynamic_fsync: code changes"
    
    This reverts commit a5c27b12bc2374386b635481ff27e2b56ae418e7.
    
    Revert "fs: dynamic_fsync: convert to state notifier"
    
    This reverts commit 9d7be66062a6cc567514d132258822c7843688ac.
    
    Revert "fs/dyn_sync_cntrl: dynamic sync control 2.0 for msm8994"
    
    This reverts commit db36ca257eccbbc44cfbb6487ee48a43c3cb0d4a.

--- fs/sync.c
+++ fs/sync.c
@@ -17,10 +17,6 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-#include <linux/dyn_sync_cntrl.h>
-#endif
-
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
 
@@ -92,25 +88,6 @@ static void fdatawait_one_bdev(struct block_device *bdev, void *arg)
 {
 	filemap_fdatawait(bdev->bd_inode->i_mapping);
 }
-
-
-#ifdef CONFIG_DYNAMIC_FSYNC
-/*
- * Sync all the data for all the filesystems using native code form sync syscall
- */
-void sync_filesystems(void)
-{
-	int nowait = 0, wait = 1;
-
-	wakeup_flusher_threads(0, WB_REASON_SYNC);
-	iterate_supers(sync_inodes_one_sb, NULL);
-	iterate_supers(sync_fs_one_sb, &nowait);
-	iterate_supers(sync_fs_one_sb, &wait);
-	iterate_bdevs(fdatawrite_one_bdev, NULL);
-	iterate_bdevs(fdatawait_one_bdev, NULL);
-}
-#endif
-
 
 /*
  * Sync everything. We start by waking flusher threads so that most of
@@ -200,14 +177,8 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
-	struct inode *inode;
+	struct inode *inode = file->f_mapping->host;
 
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && suspend_active))
-		return 0;
-#endif
-
-	inode = file->f_mapping->host;
 	if (!file->f_op->fsync)
 		return -EINVAL;
 	if (!datasync && (inode->i_state & I_DIRTY_TIME)) {
@@ -249,19 +220,11 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && suspend_active))
-		return 0;
-#endif
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && suspend_active))
-		return 0;
-#endif
 	return do_fsync(fd, 1);
 }
 
@@ -320,11 +283,6 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 	struct address_space *mapping;
 	loff_t endbyte;			/* inclusive */
 	umode_t i_mode;
-
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && suspend_active))
-		return 0;
-#endif
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
@@ -406,9 +364,5 @@ out:
 SYSCALL_DEFINE4(sync_file_range2, int, fd, unsigned int, flags,
 				 loff_t, offset, loff_t, nbytes)
 {
-#ifdef CONFIG_DYNAMIC_FSYNC
-	if (likely(dyn_fsync_active && suspend_active))
-		return 0;
-#endif
 	return sys_sync_file_range(fd, offset, nbytes, flags);
 }
